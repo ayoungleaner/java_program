bba38146cace6ca42856f0fb44127012e237d1e4
admin <714033882@qq.com>
2019-05-21 11:18:36 +0800

    seven commit

--- production/process.java
+++ production/process.java
@@ -9,31 +9,32 @@ public class process {
 		set_top_box stb = new set_top_box();
 		int count = 0;
 
-		ra.start();//开机
-		for(int i=0;i<++i;){
-		ra.move1();//发现机顶盒，给机顶盒各个属性赋值
-		//判断是否合格，合格直接跳过
-		if(stb.isFlag()==true){
-		count++;
-	    continue;
-		}
-		
-		if(ra.getinfo(stb.getID())==false){//抓取扫描，需要判断扫描信息类型是否符合
-		continue;
-		}
-		int info=ra.saveinfo(stb.getID());//存储信息
-		ra.move2();//抓机顶盒到置具仓
-		ra.rotate_hand1();//旋转插入电源
-		ra.rotate_move();//抓取移至缓存空余区，后需要一个判断机顶盒工作时间
-		//if(stb_work_time>10)...,else,continue
-		ra.move2();//抓取抓机顶盒到置具仓
-		ra.rotate_hand2();//旋转抓手，插入接口
-		if(ra.checkinfo(info, stb.getID())==false){//核对信息，如果错误转入不合格区
-		continue;
-		}
-		ra.release();//拔出接口和电源
-		ra.rorate_hand3();//旋转抓手，抓取移至皮带轮
-		count++;
+		ra.start();// 开机
+		for (int i = 0; i < ++i;) {
+			ra.move1();// 发现机顶盒，给机顶盒各个属性赋值
+			// 判断是否合格，合格直接跳过
+			if (stb.isFlag() == true) {
+				count++;
+				continue;
+			}
+			// 判断扫描信息是否正确
+			if (ra.getinfo(stb.getID()) == false) {
+				continue;
+			}
+			int info = ra.saveinfo(stb.getID());// 存储信息
+			ra.move2();// 抓机顶盒到置具仓
+			ra.rotate_hand1();// 旋转插入电源
+			ra.rotate_move();// 抓取移至缓存空余区，后需要一个判断机顶盒工作时间
+			// if(stb_work_time>10)...,else,continue
+			ra.move2();// 抓取抓机顶盒到置具仓
+			ra.rotate_hand2();// 旋转抓手，插入接口
+			// 核对信息
+			if (ra.checkinfo(info, stb.getID()) == false) {
+				continue;
+			}
+			ra.release();// 拔出接口和电源
+			ra.rorate_hand3();// 旋转抓手，抓取移至皮带轮
+			count++;
 		}
 		System.out.println("共生产了" + count + "个");
 	}
--- production/robotic_arm.java
+++ production/robotic_arm.java
@@ -26,7 +26,7 @@ public class robotic_arm {
 	}
 
 	public boolean getinfo(int stb_id) {
-		if (stb_id > 0 ) {
+		if (stb_id > 0) {
 			System.out.println("抓取，扫描成功");
 			return true;
 		} else {
@@ -34,12 +34,15 @@ public class robotic_arm {
 			return false;
 		}
 	}
-	public int saveinfo(int stb_id){
+
+	public int saveinfo(int stb_id) {
 		return stb_id;
 	}
-    public static void move2(){
-    	System.out.println("移动至置具仓，释放，置具仓夹紧");
-    }
+
+	public static void move2() {
+		System.out.println("移动至置具仓，释放，置具仓夹紧");
+	}
+
 	public static void rotate_hand1() {
 		System.out.println("旋转抓手，插入电源");
 	}
@@ -47,22 +50,26 @@ public class robotic_arm {
 	public static void rotate_move() {
 		System.out.println("旋转抓手，抓取机顶盒，移至空余缓冲区");
 	}
-	public static void rotate_hand2(){
+
+	public static void rotate_hand2() {
 		System.out.println("旋转抓手，插入接口模块");
 	}
-	public boolean checkinfo(int save_id,int stb_id){
-		if(save_id==stb_id){
+
+	public boolean checkinfo(int save_id, int stb_id) {
+		if (save_id == stb_id) {
 			System.out.println("核对无误");
 			return true;
-		}else{
+		} else {
 			System.out.println("核对有误，移至不合格区");
 			return false;
-		}	
+		}
 	}
-	public static void release(){
+
+	public static void release() {
 		System.out.println("拔出接口模块，拔出电源");
 	}
-	public static void rorate_hand3(){
+
+	public static void rorate_hand3() {
 		System.out.println("旋转抓手，抓取机顶盒到皮带轮");
 	}
 
