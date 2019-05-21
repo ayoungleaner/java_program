6d76a05c34fe361a49117ebcc225912d47e8d281
admin <714033882@qq.com>
2019-05-21 11:07:59 +0800

    seventh commit

--- production/process.java
+++ production/process.java
@@ -5,27 +5,27 @@ public class process {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		robotic_arm  ra=new robotic_arm(1);
-		set_top_box  stb=new set_top_box();
-		stb.setStb_site(10);
-		int count=0;
-		
+		robotic_arm ra = new robotic_arm(1);
+		set_top_box stb = new set_top_box();
+		int count = 0;
+
 		ra.start();
-		
+        for(int i=0;i<++i;){
+        	int l=(int)(Math.random()*20);
+        	System.out.println(l);
+        	stb.setStb_site(l);
 		ra.move1();
-		
-		if(ra.get(stb.getStb_site())==true){
+
+		if (ra.get(stb.getStb_site()) == true) {
 			ra.move2();
 			ra.release();
 			count++;
-		}else{
+		} else {
 			ra.stop();
+			break;
 		}
-		System.out.println("共生产了"+count+"个");		
+        }
+		System.out.println("共生产了" + count + "个");
 	}
-
-	
-
-	
 
 }
--- production/robotic_arm.java
+++ production/robotic_arm.java
@@ -1,42 +1,50 @@
-
 public class robotic_arm {
-      private int ID;
-      public robotic_arm(){
-    	  
-      }
-      public robotic_arm(int ID){
-    	  this.ID=ID;
-      }
+	private int ID;
+
+	public robotic_arm() {
+
+	}
+
+	public robotic_arm(int ID) {
+		this.ID = ID;
+	}
+
 	public int getID() {
 		return ID;
 	}
+
 	public void setID(int iD) {
 		ID = iD;
 	}
-      public static void start(){
-    	  System.out.println("正在开机");
-      }
-      public static void move1(){
-    	  System.out.println("移动到位置1");
-      }
-      public boolean get(int x){
-    	  if(x>0&&x<20){
-    		  System.out.println("抓取成功");
-    		  return true;
-    	  }else{
-    		  System.out.println("抓取失败");
-    		  return false;
-    	  }
-      }
-      public static void move2(){
-    	  System.out.println("移动到位置2");
-      }
-      public static void release(){
-    	  System.out.println("释放物体");
-      }
-      public static void stop(){
-    	  System.out.println("已经停止");
-      }
-      
-      
+
+	public static void start() {
+		System.out.println("正在开机");
+	}
+
+	public static void move1() {
+		System.out.println("移动到位置1");
+	}
+
+	public boolean get(int x) {
+		if (x > 0 && x < 20) {
+			System.out.println("抓取成功");
+			return true;
+		} else {
+			System.out.println("抓取失败");
+			return false;
+		}
+	}
+
+	public static void move2() {
+		System.out.println("移动到位置2");
+	}
+
+	public static void release() {
+		System.out.println("释放物体");
+	}
+
+	public static void stop() {
+		System.out.println("已经停止");
+	}
+
 }
